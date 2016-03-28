76a198d13507d33e8ea0dc3846138bc60f9c3482
Jonathan Fuqua <jfuqua390@gmail.com>
2016-03-28 13:03:48 -0500

    Create Seriesinfoview and pull in API in index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 'use strict';
+import SeriesInfoView from 'series-info-view';
 
 export default function () {
   // Look up our todos from the server
-  // fetch(`http://gateway.marvel.com:80/v1/public/series/13744?apikey=3483ce45801a173a8d3e9f5dafe775c6`);
+  fetch(`http://gateway.marvel.com:80/v1/public/series/2021?apikey=3483ce45801a173a8d3e9f5dafe775c6`)
     // Wait for all of the text to come back from the server
     //   parse text from the server as JSON
-    // .then((response) => response.json())
+    .then((response) => response.json())
     // // Take the JSON parsed data
-    // .then((data) => {
-      // Create a new TodoList
-      //
-      // Show info from the list
-
+    .then((info) => {
+      // Select element
+      const sidebar = document.querySelector(`.sidebar`);
+      // throw into element
+      const prof = new SeriesInfoView(sidebar, info.data.results[0]);
+    });
 }
--- src/series-info-view.js
+++ src/series-info-view.js
@@ -1,7 +1,34 @@
 
 export default class SeriesInfoView {
-  constructor(el, seriesid) {
+  constructor(el, data) {
     this.el = el;
-    this.seriesid = seriesid;
+    this.data = data;
+
+    this.renderImage();
+    this.renderTitle();
+    this.renderYears();
+    this.renderCreators();
+  }
+
+  renderImage() {
+    this.el.querySelector(`.sidebar__pic`).innerHTML =
+    `<img src="${this.data.thumbnail.path}.${this.data.thumbnail.extension}"
+     alt="No Img found" className="sidebar__img"/>`;
+  }
+
+  renderTitle() {
+    this.el.querySelector(`.sidebar__title`).innerText = this.data.title;
+  }
+
+  renderYears() {
+    this.el.querySelector(`.sidebar__years`).innerText = `${this.data.startYear} - ${this.data.endYear}`;
+  }
+
+  renderCreators() {
+    this.data.creators.items.forEach((creator) => {
+      const newListItem = document.createElement(`li`);
+      newListItem.innerText = creator.name;
+      this.el.querySelector(`.sidebar__creator-list`).appendChild(newListItem);
+    });
   }
 }
